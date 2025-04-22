dd3f2dfc977f97b9a351f41cd208bc0510e55b72
neojarvis <techadmin@iamneo.ai>
2025-04-22 11:24:15 +0000

    Submit-project-save-commit-22/04/2025--16:54:13

---
+++ dotnetapp/Models/Store.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace dotnetapp.Models
+{
+    public class Store
+    {
+        public int id { get; set; }
+        public string ProcuctName { get; set; }
+        public DateTime Category { get; set; }
+        public string Price { get; set; }
+             
+    }
+}
